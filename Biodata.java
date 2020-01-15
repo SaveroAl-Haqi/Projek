d7709bffc58d3597fc597db026a2ee44c9a5b882
SaveroAl-Haqi <yusaalhaqi22@gmail.com>
2020-01-15 09:14:19 +0700

    project 4

---
+++ Biodata.java
@@ -0,0 +1,41 @@
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package contoh;
+
+import java.util.Scanner;
+
+public class Biodata {
+
+    public static void main(String[] args) {
+        //deklarasi variable
+        String Nama;
+        String Nim;
+        String Kelas;
+        String Alamat;
+        //memberikan nilai pada variable secara dinamis
+        //memanggil class scanner
+        Scanner input_bio=new Scanner (System.in);
+        //nama
+        System.out.print("Nama:");
+        Nama="Riyan";
+        //nim
+        System.out.print("Nim:");
+        Nim="1917";
+        //Kelas
+        System.out.print("Kelas:");
+        Kelas="12";
+        //Alamat
+        System.out.print("Alamat:");
+        Alamat="Jakarta";
+        //cetak nama, nim, kelas, dan alamat
+        System.out.println("Nama"+Nama);
+        System.out.println("Nim"+Nim);
+        System.out.println("Kelas"+Kelas);
+        System.out.println("Alamat"+Alamat);
+    }
+    
+}
